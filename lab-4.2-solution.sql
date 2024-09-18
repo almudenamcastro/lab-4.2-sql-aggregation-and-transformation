abdd2ddbcd3772bfaf4efa38a78a443633226492
Almudena M. Castro <almu@Almudenas-MacBook-Pro.local>
2024-09-18 15:07:44 +0200

    typo correction

--- lab-4.2-solution.sql
+++ lab-4.2-solution.sql
@@ -2,7 +2,7 @@
 
 -- 1. You need to use SQL built-in functions to gain insights relating to the duration of movies:
 	-- 1.1 Determine the **shortest and longest movie durations** and name the values as `max_duration` and `min_duration`.
-SELECT MIN(sf.length) as min_duration, MAX(length) as max_duration
+SELECT MIN(sf.length) as min_duration, MAX(sf.length) as max_duration
 FROM sakila.film as sf;
 
 	-- 1.2. Express the **average movie duration in hours and minutes**. Don't use decimals.
